563c8da8e79f02c16eff2ea5c834dfdc1e713a92
a35506322 <andy28572463@gamil.com>
2021-05-30 16:31:03 +0800

    add vue cli vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    // 可以選用上面import方式注入或者採用這方式
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/newPage',
+    name: '新增頁面',
+    component: () => import('../views/NewPage.vue')
   }
 ]
 
@@ -22,4 +28,5 @@ const router = createRouter({
   routes
 })
 
+// 輸出給其他頁面載入
 export default router
