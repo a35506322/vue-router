d1beb12056fcaa29a2c5558b006c5433940db8e4
a35506322 <andy28572463@gamil.com>
2021-05-30 18:19:18 +0800

    add bs nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/newPage">新增頁面</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/newPage">新增頁面</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+      <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
